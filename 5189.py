70dcac9d662347595a9669fa9188e6ecef6e8085
Dong-Young Kim <31337.persona@gmail.com>
2020-05-16 15:25:17 +0900

    add 5189

---
+++ 5189.py
@@ -0,0 +1,29 @@
+# 전자키트
+
+import itertools
+
+T = int(input())
+N = 0
+
+
+def search(table, numbers, index):
+    global N
+
+    x = numbers[index - 1]
+
+    if index == N - 1:
+        return table[x][0] + table[0][numbers[0]]
+
+    y = numbers[index]
+
+    return table[x][y] + search(table, numbers, index + 1)
+
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    table = [list(map(int, input().split())) for _ in range(N)]
+
+    p = itertools.permutations([x for x in range(1, N)], N - 1)
+    m = min([search(table, list(l), 1) for l in list(p)])
+
+    print(f"#{test_case} {m}")
