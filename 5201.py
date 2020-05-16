0d4e2ec6ffd1f18d7e808bcbc072620f18a997e2
Dong-Young Kim <31337.persona@gmail.com>
2020-05-16 15:25:52 +0900

    add 5201

---
+++ 5201.py
@@ -0,0 +1,21 @@
+# 컨테이너 운반
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N, M = list(map(int, input().split()))
+    w = list(map(int, input().split()))
+    t = list(map(int, input().split()))
+
+    w.sort(reverse=True)
+    t.sort(reverse=True)
+
+    l = []
+    for m in t:
+        for n in w:
+            if m >= n:
+                l.append(n)
+                w.remove(n)
+                break
+
+    print(f"#{test_case} {sum(l)}")
