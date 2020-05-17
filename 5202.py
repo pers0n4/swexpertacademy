ee24481d83b99eb428809bdcecacd0355278bfe4
Dong-Young Kim <31337.persona@gmail.com>
2020-05-17 20:34:12 +0900

    feat(5202): add 5202

---
+++ 5202.py
@@ -0,0 +1,22 @@
+# 화물 도크
+# pass
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    l = [tuple(map(int, input().split())) for _ in range(N)]
+
+    l.sort(key=lambda x: x[1])
+
+    queue = []
+    for i, (_, e) in enumerate(l):
+        if queue:
+            if queue[len(queue) - 1][1] > l[i][0]:
+                continue
+        for s, _ in l[i + 1 :]:
+            if s >= e:
+                break
+        queue.append(l[i])
+
+    print(f"#{test_case} {len(queue)}")
