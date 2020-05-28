fd6968d4255defea191083bf82bed4a17ec1687c
Dong-Young Kim <31337.persona@gmail.com>
2020-05-28 23:39:33 +0900

    feat(5253): add 5253

---
+++ 5253.py
@@ -0,0 +1,18 @@
+# 접두어 검색
+# 10/10
+
+T = int(input())
+for test_case in range(1, T + 1):
+    N, M = list(map(int, input().split()))
+    A = [input() for _ in range(N)]
+    B = [input() for _ in range(M)]
+
+    is_prefix = [False] * M
+    for i, b in enumerate(B):
+        for a in A:
+            if is_prefix[i]:
+                break
+            if a.startswith(b):
+                is_prefix[i] = True
+
+    print(f"#{test_case} {sum(is_prefix)}")
